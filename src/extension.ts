69d73874fa5535a9938fbe493d6bb78bce79b1e2
Bognaum <bognaum@gmail.com>
2022-06-26 00:38:15 +0300

    improved

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,10 @@
 import * as vsc from 'vscode';
 export function activate(context: vsc.ExtensionContext) {
 	const commands = [
-		// vsc.commands.registerCommand('command', () => {}),
-		vsc.commands.registerTextEditorCommand('get-commands.getCommands', (tEditor: vsc.TextEditor, edit: vsc.TextEditorEdit, ...args: any[]) => {
-			vsc.commands.getCommands(true).then(
-				(arr) => {
-					vsc.workspace.openTextDocument({
-						content: arr.sort().join("\n")
-					}).then ((doc) => {
-						vsc.window.showTextDocument(doc);
-					});
-				}
-			);
+		vsc.commands.registerCommand('get-commands.getCommands', () => {
+			vsc.commands.getCommands(true)
+			.then((arr) => vsc.workspace.openTextDocument({content: arr.sort().join("\n")}))
+			.then ((doc) =>  vsc.window.showTextDocument(doc));
 		}),
 	];
 
